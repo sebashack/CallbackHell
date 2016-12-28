eadf242f646cb22e8fbf24fedf56b9cc89717d86
sebashack <sebastian0092@gmail.com>
2016-12-27 22:20:47 -0500

    adding some parallelism in main

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,27 @@
 module Main where
 
 import Prelude
+import Control.Monad.Cont.Trans (runContT)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Parallel.Class (parallel, sequential)
+import ReadWrite (readFileContEx)
+import FileIO (FS)
+import Control.Monad.Except (runExceptT)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+
+
+-- This is an example of a parallel computation using the Parallel
+-- type class and the power of Applicatives.
+main :: forall e. Eff (fs :: FS, console :: CONSOLE | e) Unit
+main = (flip runContT) logShow (runExceptT compt)
+  where
+    compt = let t1 = "/tmp/file1.txt"
+                t2 = "/tmp/file2.txt"
+            in sequential $
+                 append <$> parallel (readFileContEx t1) <*> parallel (readFileContEx t2)  
+
+
+
+
+    
